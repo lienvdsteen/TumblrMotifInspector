b02c6507cf18fd522fec6d03d40efadde86b649e
lienvdsteen <lienvandensteen@gmail.com>
2013-03-23 20:48:16 +0100

    first steps in the createStats()

--- app/protected/class/TumblrCalculator.php
+++ app/protected/class/TumblrCalculator.php
@@ -1,5 +1,21 @@
 <?php
 
 class TumblrCalculator {
-	
+
+	public static function createStats($posts) {
+		if (!is_array($posts) || empty($posts)) {
+			return false;
+		}
+
+		$types = array();
+		foreach ($posts as $post) {
+			if (!isset($types[$post['type']])) {
+				$types[$post['type']] = 1;
+			}
+			else {
+				$types[$post['type']]++;
+			}
+		}
+		return $types;
+	}
 }
